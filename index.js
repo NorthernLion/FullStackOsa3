4cd3a9fd6321844ac9bdb1c446c2043a0ff42018
NorthernLion <joppetaja@gmail.com>
2018-01-25 18:12:41 +0200

    3.5

--- index.js
+++ index.js
@@ -53,6 +53,26 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end()
 })
 
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+
+  persons = persons.concat(person)
+
+  res.json(person)
+})
+
+const generateId = () => {
+  return (Math.random() * Math.floor(9999999999999999))
+}
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
