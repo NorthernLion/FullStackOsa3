f583345fcd22fdb11c7f1257f5ca6022f0dc8808
NorthernLion <joppetaja@gmail.com>
2018-01-25 17:49:13 +0200

    3.3

--- index.js
+++ index.js
@@ -35,6 +35,17 @@ app.get('/info', (req, res) => {
   res.send(`<p>puhelinluettelossa on ${persons.length} henkilön tiedot</p> ${new Date()}`)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
